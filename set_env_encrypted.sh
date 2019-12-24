27d488cf032efb4903d39c4dc306408da8681942
nquery512 <nquery512@gmail.com>
2019-12-24 11:18:39 +0000

    Better env

--- set_env_encrypted.sh
+++ set_env_encrypted.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-# DONT COMMIT THIS VERSION
-export EXTERNAL_API_KEY=JhTtaSQXC6hD2HOELggq4d4Qc
+export EXTERNAL_API_KEY=JhTtaSQXC6hD2HOELgq4d4Qc
 export EXTERNAL_API_URL=https://data.cityofchicago.org/resource/t2qc-9pjd.json
 
 export PROJECT="lucid-destiny-262216"
