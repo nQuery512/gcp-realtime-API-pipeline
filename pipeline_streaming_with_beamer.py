f3c44386a8daac2f06a50909ec14685205c8ec5d
nquery512 <nquery512@gmail.com>
2019-12-24 23:44:42 +0000

    Working Apache Beam pipeline

--- pipeline_streaming_with_beamer.py
+++ pipeline_streaming_with_beamer.py
@@ -10,17 +10,28 @@ import os
 import json
 
 PROJECT = os.environ['PROJECT']
-schema = 'region:STRING, region_id:INTEGER, _description:STRING, current_speed:FLOAT, _last_updt:DATETIME, _west:STRING, _east:STRING, _south:STRING, _north:STRING'
+schema = 'region:STRING, region_id:INTEGER, _description:STRING, current_speed:FLOAT, _last_updt:DATETIME, _west:FLOAT, _east:FLOAT, _south:FLOAT, _north:FLOAT'
 TOPIC_NAME = os.environ['TOPIC_NAME']
 DATABASE_NAME = os.environ['DATABASE_NAME']
 TABLE_NAME = os.environ['TABLE_NAME']
 TOPIC = "projects/"+PROJECT+"/topics/"+TOPIC_NAME
 print(TOPIC)
 
-class PrintData(beam.DoFn):
+DB_URI = PROJECT+':'+DATABASE_NAME+'.'+TABLE_NAME
+print("\n\n\n"+DB_URI+"\n\n\n")
+class ParseData(beam.DoFn):
+
     def process(self, element):
-        print(element)
-        return element
+        res = []
+        #  print(element)
+        json_data = json.loads(element)
+        json_data = json_data['messages']
+        for elem in json_data:
+#            print(elem['data'])
+            res.append(elem['data'])
+        print(type(res))
+        print(res)
+        return res
 
 def main(argv=None):
 
@@ -35,7 +46,7 @@ def main(argv=None):
    (p
       | 'ReadData' >> beam.io.ReadFromPubSub(topic=TOPIC).with_output_types(bytes)
       | "Decode" >> beam.Map(lambda x: x.decode('utf-8'))
-      | "ParseJSON" >> beam.Map(json.loads)
+      | "ParseJSON" >> beam.ParDo(ParseData())
 #      | "Print Data" >> beam.Map(PrintData)
      # | "Clean Data" >> beam.Map(json.dumps)
      # | 'ParseCSV' >> beam.ParDo(Split())
